f30a82de15da7c1d3ef42e40b69643047af8ad40
Bora <bora@Bora-MacBook-Air.local>
2023-01-21 20:46:18 +0300

    finished postRoutes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -7,5 +7,40 @@ import Post from '../mongodb/models/post.js';
 dotenv.config();
 
 const router = express.Router();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// GET /api/v1/posts
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (error) {
+    res.status(404).json({ success: false, message: error.message });
+  }
+});
+
+// POST /api/v1/posts
+router.post('/', async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo, {
+      upload_preset: 'dalle',
+    });
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.secure_url,
+    });
+
+    res.status(201).json({ success: true, data: newPost });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 export default router;
